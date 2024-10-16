ac31ca3c26f9ca64e4fa83cb01298b65750c3479
Vera Laísa Arruda <vera.arruda@ipam.org.br>
2024-10-16 18:10:25 -0300

    Update B_1_0_gee_gui_collect_samples_burned_area_classification.py

--- network/classification_algorithms/B_1_0_gee_gui_collect_samples_burned_area_classification.py
+++ network/classification_algorithms/B_1_0_gee_gui_collect_samples_burned_area_classification.py
@@ -2,12 +2,12 @@
 presentation_link = "https://docs.google.com/presentation/d/1iMRXRH4xoWTFPSSzDOJkB6c7KJLPggxrnvYzVQ3BzP0/edit#slide=id.g220825c6698_0_546"
 gee_toolkit_link = "https://code.earthengine.google.com/?scriptPath=users%2Fmapbiomasworkspace1%2Fmapbiomas-fire%3A1-Toolkit_Collection1%2FToolkit_samples_collection"
 
-# Função que imprime os labels e os links
+# Função que imprime os labels e os links || Function that prints the labels and links
 def display_links():
-    print("### Como funciona e como usar o toolkit ###")
-    print(f"Apresentação: {presentation_link}")
-    print("\n### Acesse o Toolkit no GEE ###")
-    print(f"Link do Toolkit: {gee_toolkit_link}")
+    print("### How it works and how to use the toolkit ###")
+    print(f"Presentation: {presentation_link}")
+    print("\n### Access the Toolkit on GEE ###")
+    print(f"Toolkit Link: {gee_toolkit_link}")
 
 # Execução principal
 if __name__ == "__main__":
