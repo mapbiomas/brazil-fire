89af685537d8e07630ccecfa4940a3496fdd5926
Wallace Vieira da Silva <49287183+wallyboy22@users.noreply.github.com>
2024-10-22 13:58:32 -0300

    Update A_0_0_oficial_gitclone.py

--- network/classification_algorithms/A_0_0_oficial_gitclone.py
+++ network/classification_algorithms/A_0_0_oficial_gitclone.py
@@ -1,4 +1,4 @@
-# 2024/10/22 - mapbiomas/brazil-fire/network - source: IPAM - contact: contato@mapbiomas.org
+# last_update: '2024/10/22', github:'mapbiomas/brazil-fire', source: 'IPAM', contact: 'contato@mapbiomas.org'
 # MapBiomas Fire Classification Algorithms Step A_0_0_oficial_gitclone.py 
-### Clone the official MapBiomas Fire Network GitHub repository to the temporary disk of Google Colab
+### Step A_0_0: Clone the official MapBiomas Fire Network GitHub repository to the temporary disk of Google Colab
 !git clone https://github.com/mapbiomas/brazil-fire
