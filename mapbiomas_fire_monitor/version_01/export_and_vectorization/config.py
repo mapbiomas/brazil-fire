1d47d7e69e7ec0f090993a2246839c6ff2ec2925
Marcos Rosa <mrosa@arcplan.com.br>
2026-09-21 12:44:19 -0300

    Remove water collection configuration
    Removed water collection configuration from the config file.

--- mapbiomas_fire_monitor/version_01/export_and_vectorization/config.py
+++ mapbiomas_fire_monitor/version_01/export_and_vectorization/config.py
@@ -1077,8 +1077,6 @@ OBJ = {
                 _p("ecuador", "water", "collection1", "frequency",
                    "projects/mapbiomas-public/assets/ecuador/collection1/mapbiomas_ecuador_collection1_water_frequency_v1", "byte"),
             ],
-        },
-        "water": {
             "collection_04": [
                 _p("ecuador", "water", "collection1", "annual_water_coverage",
                    "projects/mapbiomas-public/assets/ecuador/water/collection4/mapbiomas_ecuador_collection4_water_surface_v1", "byte"),
